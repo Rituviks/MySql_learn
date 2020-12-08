c51d70a83edc492ef3daee3d95d43e381f23995a
Rituviks <42962288+Rituviks@users.noreply.github.com>
2020-12-08 22:17:11 +0530

    Add files via upload
    Inserting data into the table is under "Data Definition Language"(DDL).

---
+++ inserting value.sql
@@ -0,0 +1,8 @@
+insert into fish(name,type,age)
+	values('tiger barb','wild',2),
+          ('goldfish','soft',1),
+          ('fighter','wild',1),
+          ('Oscar','wild',2),
+          ('Turtle','land',1),
+          ('Tinfoild','wild',1),
+          ('sucker','soft',1);
